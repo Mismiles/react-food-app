e46080d9a76ca24be0307c8a62486b096b10d886
Msmiles <muhammadirfan123@hotmail.com>
2021-08-02 12:04:35 +0000

    added styling to header, div and image of Header.js file

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,21 @@
 import {Fragment} from 'react';
 
+import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = props => {
-    return <Fragment>
-        <header className={classes.Header}>
+    return (
+        <Fragment>
+        <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
-        <div>
+        <div  className={classes['main-image']}>
             <img src={mealsImage} alt="A table full of food"/>
         </div>
     </Fragment>
+    );
 };
 
 export default Header;
